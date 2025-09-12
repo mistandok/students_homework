51ea8137adffa1c8c5ff7483a5852286e933a137
akartikov <akartikov@avito.ru>
2025-09-12 09:45:53 +0300

    добавил заглушки для тестов

--- 4_decorators/retry/retry.py
+++ 4_decorators/retry/retry.py
@@ -1,2 +1,10 @@
 # реализуйте декоратор вида @retry(count: int, delay: timedelta, handled_exceptions: tuple[type(Exceptions)])
 
+from datetime import timedelta
+
+
+def retry(count: int, delay: timedelta, handled_exceptions: tuple = (Exception,)):
+    """Декоратор для повторных попыток выполнения функции."""
+    # TODO: реализовать декоратор
+    pass
+
